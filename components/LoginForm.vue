f811f43418fcd2f448e35726641cacade2a28d14
Lakshan Vidanapathirana <rlvidanapathirana@gmail.com>
2022-01-31 15:06:40 +0530

    alignment

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,53 +1,76 @@
 <template>
   <div>
     <a-form :form="form" @submit="handleSubmit">
-    <a-form-item :validate-status="userNameError() ? 'error' : ''" :help="userNameError() || ''">
-      <a-input
-        size="large"
-        v-decorator="[
-          'userName',
-          { rules: [{ required: true, message: 'Please input your username!' }] },
-        ]"
-        placeholder="Username"
-        
+      <a-form-item
+        :validate-status="userNameError() ? 'error' : ''"
+        :help="userNameError() || ''"
       >
-        <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item :validate-status="passwordError() ? 'error' : ''" :help="passwordError() || ''">
-      <a-input-password
-        size="large"
-        v-decorator="[
-          'password',
-          { rules: [{ required: true, message: 'Please input your Password!' }] },
-        ]"
-        type="password"
-        placeholder="Password"
+        <a-input
+          size="large"
+          v-decorator="[
+            'userName',
+            {
+              rules: [
+                { required: true, message: 'Please input your username!' },
+              ],
+            },
+          ]"
+          placeholder="Username"
+        >
+          <a-icon
+            slot="prefix"
+            type="user"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item
+        :validate-status="passwordError() ? 'error' : ''"
+        :help="passwordError() || ''"
       >
-        <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
-      </a-input-password>
-    </a-form-item>
-    <a-button type="link">Forgot Password?</a-button>
-    <a-form-item>
-      <a-button type="primary ms-3" html-type="submit" :disabled="hasErrors(form.getFieldsError())">
-        Log in
-      </a-button>
-    </a-form-item>
-
-  </a-form>
-
+        <a-input-password
+          size="large"
+          v-decorator="[
+            'password',
+            {
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+              ],
+            },
+          ]"
+          type="password"
+          placeholder="Password"
+        >
+          <a-icon
+            slot="prefix"
+            type="lock"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input-password>
+      </a-form-item>
+      <a-button type="link">Forgot Password?</a-button>
+      <a-form-item>
+        <a-button
+          type="primary ms-3"
+          html-type="submit"
+          :disabled="hasErrors(form.getFieldsError())"
+        >
+          Log in
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
 function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
+  return Object.keys(fieldsError).some((field) => fieldsError[field]);
 }
 export default {
   data() {
     return {
       hasErrors,
-      form: this.$form.createForm(this, { name: 'horizontal_login' }),
+      form: this.$form.createForm(this, { name: "horizontal_login" }),
     };
   },
   mounted() {
@@ -60,19 +83,19 @@ export default {
     // Only show error after a field is touched.
     userNameError() {
       const { getFieldError, isFieldTouched } = this.form;
-      return isFieldTouched('userName') && getFieldError('userName');
+      return isFieldTouched("userName") && getFieldError("userName");
     },
     // Only show error after a field is touched.
     passwordError() {
       const { getFieldError, isFieldTouched } = this.form;
-      return isFieldTouched('password') && getFieldError('password');
+      return isFieldTouched("password") && getFieldError("password");
     },
     handleSubmit(e) {
       e.preventDefault();
-      window.location.href="/home"
+      window.location.href = "/home";
       this.form.validateFields((err, values) => {
         if (!err) {
-          console.log('Received values of form: ', values);
+          console.log("Received values of form: ", values);
         }
       });
     },
